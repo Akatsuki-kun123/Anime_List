c3947c4b192d7b7f1affbc88678cd4c3c36e3707
Akatsuki <tm09102003@gmail.com>
2023-05-23 10:19:44 +0700

    modify data file

--- src/Constants/core.js
+++ src/Constants/core.js
@@ -3,28 +3,37 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 var data = [];
-var JPname = "";
-var ENname = "";
-var episodes = 0;
-var aired = "";
-var producers = [];
-var studios = [];
-var genres = [];
-var description = "";
+var interval = 0;
 
 class Anime {
-    constructor(JPname, ENname, episodes, aired, producers, studios, genres) {
+    constructor(JPname, ENname, image, aired, description, episodes, characters, producers, studios, genres) {
         this.JPname = JPname;
         this.ENname = ENname;
-        this.episodes = episodes;
+        this.image = image;
         this.aired = aired;
+        this.description = description;
+        this.episodes = episodes;
+        this.characters = characters;
         this.producers = producers;
         this.studios = studios;
         this.genres = genres;
-        this.description = description;
     }
 }
 
+class Character {
+    constructor(name, image, voiceActor) {
+        this.name = name;
+        this.image = image;
+        this.voiceActor = voiceActor;
+    }
+}
+
+class VoiceActor {
+    constructor(name, image) {
+        this.name = name;
+        this.image = image;
+    }
+}
 
 var headers = {
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
@@ -32,7 +41,7 @@ var headers = {
 
 function searchImages(limit) {
     request(`https://myanimelist.net/topanime.php?limit=${limit}`, {headers: headers}, (error, response, body) => {
-        let wait = 60000;
+        let wait = 0;
 
         if(!error && response.statusCode == 200) {
             const $ = cheerio.load(body);
@@ -59,13 +68,49 @@ function searchImages(limit) {
 }
 
 function getData(options) {
+    let JPname = "",
+        ENname = "",
+        image = "",
+        aired = "",
+        description = "",
+        episodes = 0,
+        characters = [],
+        producers = [],
+        studios = [],
+        genres = [];
+
     request(options, (error, response, body) => {
         if(!error && response.statusCode == 200) {
             const $ = cheerio.load(body);
+            $('div[class="detail-characters-list clearfix"]').first().find('table').map(function(index, elem) {
+                if (index % 2 == 1) {
+                    return 0;
+                }
+
+                let charName = "",
+                    charImage = "",
+                    VA = new VoiceActor();
+                
+                $(elem).find('td').map(function(index, elem) {
+                    if (index == 0) {
+                        charImage = $(elem).find('img').attr('data-src');
+                    } else if (index == 1) {
+                        charName = $(elem).find('a').text();
+                    } else if (index == 2) {
+                        VA.name = $(elem).find('a').first().text();
+                        VA.image = $(elem).find('img').attr('data-src');
+                    }
+                });
+                
+                let char = new Character(charName, charImage, VA);
+                characters.push(char);
+            });
+            
             $('div[class="spaceit_pad"]').map(function(index, elem) {
                 let stop = false;
 
                 let _data = $(elem).text();
+                
                 if (stop == true) {
                     return 0;
                 }
@@ -74,19 +119,19 @@ function getData(options) {
                     JPname = _data.split(":")[1];
                 } else if (_data.includes("English")) {
                     ENname = _data.split(":")[1];
-                } else if (_data.includes("Episode")) {
+                } else if (_data.includes("Episodes")) {
                     episodes = parseInt(_data.split(":")[1]);
                 } else if (_data.includes("Aired")) {
                     aired = _data.split(":")[1];
-                } else if (_data.includes("Producer")) {
+                } else if (_data.includes("Producers")) {
                     $(elem).find('a').map(function(index, elem) {
                         producers.push($(elem).attr("title"));
                     });
-                } else if (_data.includes("Studio")) {
+                } else if (_data.includes("Studios")) {
                     $(elem).find('a').map(function(index, elem) {
                         studios.push($(elem).attr("title"));
                     });
-                } else if (_data.includes("Genre")) {
+                } else if (_data.includes("Genres")) {
                     $(elem).find('a').map(function(index, elem) {
                         genres.push($(elem).attr("title"));
                     });
@@ -95,23 +140,20 @@ function getData(options) {
                 }
             });
 
-            let anime = new Anime(JPname, ENname, episodes, aired, producers, studios, genres);
+            image = $('div[class="leftside"]').find('img').attr('data-src');
+            description = $('p[itemprop="description"]').text();
+
+            let anime = new Anime(JPname, ENname, image, aired, description, episodes, characters, producers, studios, genres);
             data.push(anime);
 
-            JPname = "";
-            ENname = "";
-            episodes = "";
-            aired = "";
-            producers = [];
-            studios = [];
-            genres = [];
-
-            fs.writeFileSync("./src/Constants/data.json", JSON.stringify(data, null, '\t'));
+            fs.writeFileSync("data.json", JSON.stringify(data, null, '\t'));
+            
         }
     });
 }
 
 for (let limit = 0; limit < 7; limit++) {
-    searchImages(limit * 50);
+    setTimeout(() => {
+        searchImages(limit * 50);
+    }, (interval + 500000 * limit).toString());
 }
-
