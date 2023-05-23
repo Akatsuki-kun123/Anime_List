74bf507633e7178a135ff6e6d9c8d67a44a8b229
Akatsuki <tm09102003@gmail.com>
2023-05-23 22:07:51 +0700

    Add comment in some 複雑 places

--- src/Constants/core.js
+++ src/Constants/core.js
@@ -1,9 +1,10 @@
+//Thư viện cần dùng: nodejs, cheerio.
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 
-var data = [];
-var interval = 0;
+var data = [];          //Biến data lưu trữ giá trị của các Object "Anime" 
+var interval = 0;       //Biến thời gian trễ để tạo khoảng cách thời gian giữa các request
 
 class Anime {
     constructor(JPname, ENname, image, aired, description, episodes, characters, producers, studios, genres) {
@@ -35,27 +36,31 @@ class VoiceActor {
     }
 }
 
+//Tạo header cho request để tránh bị ... denied
 var headers = {
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
 };
 
+//Request lấy link các Object Anime để tiến hành lấy thông tin
 function searchImages(limit) {
     request(`https://myanimelist.net/topanime.php?limit=${limit}`, {headers: headers}, (error, response, body) => {
-        let wait = 0;
+        let wait = 0;   //Biến thời gian đợi cho mỗi request
 
         if(!error && response.statusCode == 200) {
-            const $ = cheerio.load(body);
+            const $ = cheerio.load(body);   //Lấy html của trang web
             $('.hoverinfo_trigger').map(function(index, elem) {
                 if (index % 2 == 1) {
-                    return 0;
+                    return 0;   //Loại bỏ phần trùng lặp
                 }
 
-                let link = $(elem).attr("href");
+                let link = $(elem).attr("href");    //Lấy link của cái Object Anime
+                //Tạo timeout cho các request để tránh bị timeout khi mạng lag
                 let options = {
                     url: link,
                     timeout: 1000000
                 }
 
+                //Tiến hành lấy data cho các Object Anime mỗi lần request chờ 10s
                 setTimeout(() => {
                     getData(options);
                     console.log("Pass");
@@ -67,8 +72,9 @@ function searchImages(limit) {
     });
 }
 
+//Hàm lấy thông tin cho các Object Anime
 function getData(options) {
-    let JPname = "",
+    let JPname = "",            //Tạo các biến cần thiết để lưu trữ dữ liệu
         ENname = "",
         image = "",
         aired = "",
@@ -81,16 +87,18 @@ function getData(options) {
 
     request(options, (error, response, body) => {
         if(!error && response.statusCode == 200) {
-            const $ = cheerio.load(body);
+            const $ = cheerio.load(body);   //Lấy html của trang web
+            //Lấy data cho phần thông tin "Character"
             $('div[class="detail-characters-list clearfix"]').first().find('table').map(function(index, elem) {
                 if (index % 2 == 1) {
                     return 0;
                 }
 
-                let charName = "",
+                let charName = "",          //Tạo các biến cần thiết để lưu dữ liệu
                     charImage = "",
                     VA = new VoiceActor();
                 
+                //Tiến hành lấy dữ liệu cho "Character" từ html
                 $(elem).find('td').map(function(index, elem) {
                     if (index == 0) {
                         charImage = $(elem).find('img').attr('data-src');
@@ -106,15 +114,15 @@ function getData(options) {
                 characters.push(char);
             });
             
+            //Lấy dữ liệu cho các trường còn lại
             $('div[class="spaceit_pad"]').map(function(index, elem) {
-                let stop = false;
-
-                let _data = $(elem).text();
-                
+                let stop = false;   //Biến dừng khi đạt đủ thông tin cần thiết
                 if (stop == true) {
                     return 0;
                 }
 
+                let _data = $(elem).text();     //Lấy phần text của html để đơn giản hóa việc lấy dữ liệu
+                //Tiến hành lấy dữ liệu "hơi nhiều if else"
                 if (_data.includes("Japanese")) {
                     JPname = _data.split(":")[1];
                 } else if (_data.includes("English")) {
@@ -140,8 +148,8 @@ function getData(options) {
                 }
             });
 
-            image = $('div[class="leftside"]').find('img').attr('data-src');
-            description = $('p[itemprop="description"]').text();
+            image = $('div[class="leftside"]').find('img').attr('data-src');    //Lấy thông tin ảnh của Anime
+            description = $('p[itemprop="description"]').text();                //Lấy thông tin mô tả về Anime
 
             let anime = new Anime(JPname, ENname, image, aired, description, episodes, characters, producers, studios, genres);
             data.push(anime);
